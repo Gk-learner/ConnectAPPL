65b3e162ab258d495e30e8322cb4d38a3063b8f8
Gagan <gagansasan91@yahoo.com>
2026-04-06 23:06:16 -0600

    mongourl change

--- connect-appBE/src/config/database.js
+++ connect-appBE/src/config/database.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://gagansasan1991:pJM3Pp4JIXHFJDul@cluster0.zge83.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  await mongoose.connect(process.env.MONGO_DB_URL  );
 };
-// mongodb+srv://gagandeep23ca:<db_password>@cluster0.tlnjldz.mongodb.net/
-//A98Q5o2fRMvmF2KD
-// pJM3Pp4JIXHFJDul
+
 module.exports = connectDB;
